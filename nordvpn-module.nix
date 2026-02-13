a8de6a934919fe4027b51e417b0743b4b9c01499
James Joseph <jaayjay@gmail.com>
2026-02-13 07:56:27 +0000

    Update: Latest nordvpn package and cleanup
    I've updated nordvpn to version 4.4.0 and I've also cleaned up the package to not be called myypo.  It's now got a namespace of custom.nordvpn module.
    
    I've also updated the README.md to provide instructions on this because I originally didn't explain that you had to also add in your configuration.nix to enable the service and add your group (I forgot you had to tbf).

--- nordvpn-module.nix
+++ nordvpn-module.nix
@@ -25,15 +25,15 @@
     wireguard-tools,
   }: let
     pname = "nordvpn";
-    version = "4.3.1";
+    version = "4.4.0";
 
     nordVPNBase = stdenv.mkDerivation {
       inherit pname version;
 
       src = fetchurl {
         url = "https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/n/nordvpn/nordvpn_${version}_amd64.deb";
-        hash = "sha256-oFf4uxZsucAh2yW++SQRxFx8+JdL8ZsNzWqzjJ2JqUs=";
-#        hash = "sha256-LcTQEqaP1+UeBxi+gqQAuQKKzVgzMWSb7rMEB6qc6hk=";
+#        hash = "sha256-oFf4uxZsucAh2yW++SQRxFx8+JdL8ZsNzWqzjJ2JqUs=";
+        hash = "sha256-rePBEVe6o49If5dYvIUW361E7nFqngzd+XkiOeehY7w=";
       };
 
       buildInputs = [libxml2 libidn2 libnl libcap_ng sqlite];
@@ -107,7 +107,7 @@
     }) {};
 in
   with lib; {
-    options.myypo.services.custom.nordvpn.enable = mkOption {
+    options.custom.services.nordvpn.enable = mkOption {
       type = types.bool;
       default = false;
       description = ''
@@ -118,13 +118,12 @@ in
       '';
     };
 
-    config = mkIf config.myypo.services.custom.nordvpn.enable {
+    config = mkIf config.custom.services.nordvpn.enable {
       networking.firewall.checkReversePath = false;
 
       environment.systemPackages = [nordVpnPkg];
 
       users.groups.nordvpn = {};
-      users.groups.nordvpn.members = ["myypo"];
       systemd = {
         services.nordvpn = {
           description = "NordVPN daemon.";
